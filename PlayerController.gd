798cf1b5167e75a1d64eb41c8f6cfa2b4ca06cad
Valtteri Kähärä <valtterikahara@gmail.com>
2024-04-18 17:43:56 +0300

    initialize project with existing files

---
+++ PlayerController.gd
@@ -0,0 +1,1 @@
+i have godot scene where there is a rigidbody player, player has children camera and collision shape. create control script for player. the player is a helicopter, it should be able to move up and down on the Y axis and forward and backward on the x axis, when pressing right or left the helicopter should rotate, if no button is pressed the helicopter stays still
